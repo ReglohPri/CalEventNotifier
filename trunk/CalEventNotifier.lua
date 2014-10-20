a8e3179b181711afe24f8379b4a4d797360e1e0b
reglohpri <reglohpri@29c07a1c-fc22-4776-9230-672a7cd02d06>
2014-10-20 20:24:00 +0000

    Update for WoD 6.0.2
    command line options "/CEN" added

--- trunk/CalEventNotifier.lua
+++ trunk/CalEventNotifier.lua
@@ -15,17 +15,34 @@ local L = vars.L
 
 
 EventNotifies = {}
+MsgToGuild = false
+local debug = false
+local GuildMembers = {}
+local GuildNotifies = {}
+local CommandQueue = {}
 local addonLoaded = false
 local todayCheck = true
 local MyChar = ""
+local MyRealm = ""
 local iTimerCanStart = false
 local checkTimers = false
 local lastTime = 0
+local lastGTime = 0
 local fadeTime = 0
 local fading = false
 local elapsedTime = 0
 local inCombat = false
 local invitesInCombat = false
+local iamInGuild = false
+local guildMaster = false
+local lastNotifyTimer = 0
+local lastNotifySender = ""
+local lastNotifyLeader = ""
+local iCanSendToGuild = true
+local iamSenderNow = false
+local GuildMembersLoaded = false
+
+local CEN_OFFLINE = string.format(ERR_FRIEND_OFFLINE_S, "(.+)")
 
 local frame = CreateFrame("Frame")
 frame:Hide();
@@ -48,6 +65,8 @@ frame:SetBackdrop({
 })
 
 frame:RegisterEvent("ADDON_LOADED")
+frame:RegisterEvent("CHAT_MSG_ADDON")
+frame:RegisterEvent("CHAT_MSG_SYSTEM")
 frame:RegisterEvent("CALENDAR_UPDATE_PENDING_INVITES")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:RegisterEvent("CALENDAR_UPDATE_EVENT_LIST")
@@ -93,6 +112,82 @@ okButton:SetScript("OnClick", okButton_OnClick)
 local close = CreateFrame("Button", "CloseButton", frame, "UIPanelCloseButton")
 close:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -8, -8)
 
+local function DPrint(msg)
+	if debug then
+		print("|cFF99FF99CalEventNotifier_Debug:|r "..msg)
+	end
+end
+
+local function CPrint(msg)
+	print("|cFF99FF99CalEventNotifier:|r "..msg)
+end
+
+local function GuildMessage(msg)
+	if iamInGuild then
+		SendChatMessage("CalEventNotifier: "..msg, "GUILD")
+	else
+		CPrint("Yor aren't in a guild")
+	end
+end
+
+local function AddonMessage(msg)
+	if iamInGuild then
+		SendAddonMessage("DMD_CEN", msg, "GUILD")
+	else
+		CPrint("Yor aren't in a guild")
+	end
+end
+
+local function GetGuildMemberIdx(gName, gArray)
+	local gIndex, i = 0
+
+	i = table.maxn(gArray)
+	if (i == 0) then
+		return 0
+	end
+
+	for gIndex = 1, i do
+		if (gArray[gIndex].Name == gName) then
+			return gIndex
+		end
+	end
+
+	return 0
+end
+
+local function CreateGuildMembers(gArray)
+	local maxMembers, i = 0
+	local sName, memberName, memberRealm
+
+	if iamInGuild then
+		maxMembers = GetNumGuildMembers()
+
+		if (maxMembers ~= nil) and (maxMembers > 1) then
+			for i = 1, maxMembers do
+				memberName = GetGuildRosterInfo(i)
+				sName, memberRealm = strsplit("-", memberName, 2)
+				if not memberRealm then
+					memberName = sName.."-"..string.gsub(MyRealm, " ", "")
+				end
+
+				table.insert(gArray, {Name = memberName})
+				DPrint("Insert Guild Member: "..memberName)
+			end
+
+			-- We need more than one member in a guild. Otherwise no messages would be send.
+			if (i ~= nil) and (i > 1) then
+				GuildMembersLoaded = true
+			end
+		end
+	end
+end
+
+local function GetShortName(cName)
+	local sMember, mRealm = strsplit("-", cName, 2)
+
+	return sMember
+end
+
 local function GetOwnCharIndex(cName, cArray)
 	local cIndex, i = 0
 
@@ -108,6 +203,19 @@ local function GetOwnCharIndex(cName, cArray)
 	end
 
 	return 0
+end
+
+local function InitGuildNotifies(gArray)
+	while (table.maxn(gArray) > 0) do
+		table.remove(gArray)
+	end
+end
+
+local function AddGuildNotify(gArray, aTitle, aYear, aMonth, aDay, aHour, aMin)
+	local CalendarTimeStamp = time({year = aYear, month = aMonth, day = aDay, hour = aHour, min = aMin})
+	local CalendarAlarmTime = CalendarTimeStamp - (15 * 60) -- 15 Min.
+
+	table.insert(gArray, {EventTitle = aTitle, CalendarTime = CalendarTimeStamp, CalendarAlarm = CalendarAlarmTime})
 end
 
 local function InitNotifies(cName, cArray)
@@ -164,13 +272,18 @@ function checkRemovedEvents()
 end
 
 local function CheckToday()
-local curweekday, curmonth, curday, curyear = CalendarGetDate()
-local numtodaysEvents = CalendarGetNumDayEvents(0, curday)
-local todaysevents = 0
+	local curweekday, curmonth, curday, curyear = CalendarGetDate()
+	local numtodaysEvents = CalendarGetNumDayEvents(0, curday)
+	local todaysevents = 0
+
 	MyChar = UnitName("player")
+	MyRealm = GetRealmName()
+	MyChar = MyChar.."-"..MyRealm
 	font3:SetText("")
 	iTimerCanStart = false
 	InitNotifies(MyChar, EventNotifies)
+	InitGuildNotifies(GuildNotifies)
+
 	if numtodaysEvents ~= 0 then
 		for i = 1, numtodaysEvents do
 			local title3, hour3, minute3, calendarType3, _, _, _, _, inviteStatus3, invitedBy3 = CalendarGetDayEvent(0, curday, i)
@@ -191,6 +304,10 @@ local todaysevents = 0
 						frame:Show()
 					end
 				end
+			end
+
+			if (calendarType3 == "GUILD_EVENT") then
+				AddGuildNotify(GuildNotifies, title3, curyear, curmonth, curday, hour3, minute3)
 			end
 		end
 	end
@@ -231,6 +348,40 @@ local function GetInvites()
 
 	if frame:IsShown() then
 		checkRemovedEvents()
+	end
+end
+
+local function ReInitNotifies(cArray)
+	while (table.maxn(cArray) > 0) do
+		table.remove(cArray)
+	end
+
+	CPrint(L["ResetFinish"])
+	CheckToday()
+end
+
+local function GetCmdIndex(qCmd, qArray)
+	local qIndex, i = 0
+
+	i = table.maxn(qArray)
+	if (i == 0) then
+		return 0
+	end
+
+	for qIndex = 1, i do
+		if (qArray[qIndex] == qCmd) then
+			return qIndex
+		end
+	end
+
+	return 0
+end
+
+local function AddCommand(qCmd, qArray)
+	local qIndex = GetCmdIndex(qCmd, qArray)
+
+	if (qIndex == 0) then
+		table.insert(qArray, qCmd)
 	end
 end
 
@@ -301,14 +452,155 @@ local function GetGuildEvents()
 	end
 end
 
+local function toggleMsgToGuild()
+	if iamInGuild then
+		MsgToGuild = not MsgToGuild
+		if MsgToGuild then
+			CPrint(L["MsgToGuild"])
+			if guildMaster then
+				AddonMessage("IAMLEADER-0001")
+				iCanSendToGuild = true
+			else
+				AddonMessage("IFLEADERON-0001")
+			end
+		else
+			CPrint(L["NoMsgToGuild"])
+			if guildMaster or iamSenderNow then
+				AddonMessage("NOMSGTOGUILD-0001")
+				iamSenderNow = false
+			end
+		end
+	else
+		CPrint(L["NoGuild"])
+	end
+end
+
+local function initCEN()
+	iamInGuild = IsInGuild()
+	MyChar = UnitName("player")
+	MyRealm = GetRealmName()
+	MyChar = MyChar.."-"..MyRealm
+
+	if iamInGuild then
+		CPrint(L["InGuild"])
+		guildMaster = IsGuildLeader()
+		if guildMaster then
+			CPrint(L["GuildLeader"])
+		end
+
+		CreateGuildMembers(GuildMembers)
+		if MsgToGuild then
+			CPrint(L["MsgToGuild"])
+			if guildMaster then
+				AddonMessage("IAMLEADER-0001")
+			else
+				AddonMessage("IFLEADERON-0001")
+			end
+		end
+	else
+		CPrint(L["NoGuild"])
+	end
+end
+
 local function eventHandler(self, event, ...)
 	if event == "ADDON_LOADED" then
 		local arg1 = ...;
 		if arg1 == "CalEventNotifier" then
 			addonLoaded = true
-			print("CalEventNotifier: loaded: ")
+			CPrint(L["Loaded"])
+			CPrint(L["EnterCommands"])
+
 			if not todayCheck then
+				initCEN()
 				CheckToday()
+			end
+		end
+	elseif event == "CHAT_MSG_ADDON" then
+		local prefix, msg, channel, sender = ...;
+		local absRealm, msgID, msgValue, tmpSender
+
+		if (prefix == "DMD_CEN") and (iamInGuild) then
+			sender, absRealm = strsplit("-", sender, 2)
+			msgID, msgValue = strsplit("-", msg, 2)
+
+			if (sender ~= GetShortName(MyChar)) then
+				-- CPrint Should be removed or changed to DPrint
+				DPrint("Other - msgID: "..msgID.." msgValue: "..msgValue)
+				if (msgID == "NOTIFYTIME") then
+					lastNotifySender = sender.."-"..string.gsub(MyRealm, " ", "")
+					lastNotifyTimer = msgValue
+					iCanSendToGuild = false
+					iamSenderNow = false
+				elseif (msgID == "IFLEADERON") then
+					if guildMaster and MsgToGuild then
+						AddonMessage("IAMLEADER-0001")
+					elseif iamSenderNow and MsgToGuild then
+						AddonMessage("IAMSENDERNOW-0001")
+					end
+				elseif (msgID == "IAMLEADER") then
+					lastNotifyLeader = sender.."-"..string.gsub(MyRealm, " ", "")
+
+					-- I am a guild master
+					if guildMaster then
+						iCanSendToGuild = true
+					else
+						iCanSendToGuild = false
+						iamSenderNow = false
+					end
+				elseif (msgID == "IAMSENDERNOW") then
+					lastNotifySender = sender.."-"..string.gsub(MyRealm, " ", "")
+					iCanSendToGuild = false
+					iamSenderNow = false
+				elseif (msgID == "NOMSGTOGUILD") then
+					tmpSender = sender.."-"..string.gsub(MyRealm, " ", "")
+					if (tmpSender == lastNotifySender) or (tmpSender == lastNotifyLeader) then
+						lastNotifySender = ""
+						lastNotifyTimer = 0
+						iCanSendToGuild = true
+					end
+				end
+			else
+				-- CPrint Should be removed or changed to DPrint
+				DPrint("Self - msgID: "..msgID.." msgValue: "..msgValue)
+				if (msgID == "NOTIFYTIME") then
+					iCanSendToGuild = true
+					iamSenderNow = true
+				end
+			end
+		end
+	elseif event == "CHAT_MSG_SYSTEM" then
+		local sArg1 = ...;
+		if (sArg1 ~= nil) and (iamInGuild) then
+			local _, _, gMember = string.find(sArg1, CEN_OFFLINE)
+			if (gMember ~= "") and (gMember ~= nil) then
+				local sMember, mRealm = strsplit("-", gMember, 2)
+				if not mRealm then
+					gMember = sMember.."-"..string.gsub(MyRealm, " ", "")
+				end
+
+				DPrint("CHAT_MSG_SYSTEM: "..gMember)
+
+				if not GuildMembersLoaded then
+					CreateGuildMembers(GuildMembers)
+				end
+
+				if (GetGuildMemberIdx(gMember, GuildMembers) > 0) then
+					DPrint("GuildMemberIdx: "..GetGuildMemberIdx(gMember, GuildMembers))
+					DPrint("LastNotifyLeader: "..lastNotifyLeader)
+					DPrint("LastNotifySender: "..lastNotifySender)
+					if (gMember == lastNotifySender) then
+						lastNotifySender = ""
+						lastNotifyTimer = 0
+						iCanSendToGuild = true
+						DPrint("Sender ist offline")
+					elseif (gMember == lastNotifyLeader) then
+						lastNotifyLeader = ""
+						lastNotifySender = ""
+						lastNotifyTimer = 0
+						iCanSendToGuild = true
+						DPrint("Leader ist offline")
+					end
+				end
 			end
 		end
 	elseif event == "PLAYER_ENTERING_WORLD" then
@@ -319,13 +611,14 @@ local function eventHandler(self, event, ...)
 		GetGuildEvents()
 
 		if addonLoaded then
+			initCEN()
 			CheckToday()
 		else
 			todayCheck = false
 		end
 		frame:UnregisterEvent("PLAYER_ENTERING_WORLD")
 	elseif event == "CALENDAR_UPDATE_PENDING_INVITES" or event == "CALENDAR_UPDATE_GUILD_EVENTS" or event == "CALENDAR_UPDATE_EVENT_LIST" then
-		if not inCombat then
+		if (not inCombat) and (not checkTimers) then
 			GetInvites()
 			GetGuildEvents()
 
@@ -342,6 +635,10 @@ local function eventHandler(self, event, ...)
 	elseif event == "PLAYER_REGEN_DISABLED" then
 		inCombat = true
 	end
+end
+
+if not RegisterAddonMessagePrefix("DMD_CEN") then
+	CPrint("Error: unable to register CEN addon message prefix (reached client side addon message filter limit), synchronization with guild will be unavailable")
 end
 
 frame:SetScript("OnEvent", eventHandler)
@@ -365,6 +662,8 @@ notifyTimer:SetScript("OnUpdate", function (self, elapsed)
 	local currTime = 0
 	local cIndex = 0
 	local leftTime = 0
+	local qCommand = ""
+	local tmpStr = ""
 	local oneNotify = false
 
 	if fading and (fadeTime > 0) then
@@ -390,6 +689,14 @@ notifyTimer:SetScript("OnUpdate", function (self, elapsed)
 		GetGuildEvents()
 		CheckToday()
 		invitesInCombat = false
+	end
+
+	-- Read CommandQueue at first
+	while (table.maxn(CommandQueue)) > 0 do
+		qCommand = table.remove(CommandQueue)
+		if (qCommand == "RESET") then
+			ReInitNotifies(EventNotifies)
+		end
 	end
 
 	if iTimerCanStart and not checkTimers then
@@ -460,7 +767,7 @@ notifyTimer:SetScript("OnUpdate", function (self, elapsed)
 																				EventNotifies[cInd][EventNotifies[cInd].Name][i2].EventTitle, leftTime))
 													UIFrameFadeIn(textFrame, 3, 0, 1)
 												elseif (leftTime >= 0) and not inCombat then
-													font:SetText(string.format(L["Dlg2EventStartsSoon"], EventNotifies[cInd].Name,
+													font:SetText(string.format(L["Dlg2EventStartsSoon"], GetShortName(EventNotifies[cInd].Name),
 																			   EventNotifies[cInd][EventNotifies[cInd].Name][i2].EventTitle, leftTime))
 													font2:SetText("")
 													font3:SetText("")
@@ -506,7 +813,7 @@ notifyTimer:SetScript("OnUpdate", function (self, elapsed)
 																			EventNotifies[cInd][EventNotifies[cInd].Name][i2].EventTitle, leftTime))
 												UIFrameFadeIn(textFrame, 3, 0, 1)
 											elseif (leftTime >= 0) and not inCombat then
-												font:SetText(string.format(L["Dlg2EventStartsSoon"], EventNotifies[cInd].Name,
+												font:SetText(string.format(L["Dlg2EventStartsSoon"], GetShortName(EventNotifies[cInd].Name),
 																			EventNotifies[cInd][EventNotifies[cInd].Name][i2].EventTitle, leftTime))
 												font2:SetText("")
 												font3:SetText("")
@@ -551,7 +858,7 @@ notifyTimer:SetScript("OnUpdate", function (self, elapsed)
 																	EventNotifies[cInd][EventNotifies[cInd].Name][i2].EventTitle, leftTime))
 										UIFrameFadeIn(textFrame, 3, 0, 1)
 									elseif (leftTime >= 0) and not inCombat then
-										font:SetText(string.format(L["Dlg2EventStartsSoon"], EventNotifies[cInd].Name,
+										font:SetText(string.format(L["Dlg2EventStartsSoon"], GetShortName(EventNotifies[cInd].Name),
 																   EventNotifies[cInd][EventNotifies[cInd].Name][i2].EventTitle, leftTime))
 										font2:SetText("")
 										font3:SetText("")
@@ -576,7 +883,60 @@ notifyTimer:SetScript("OnUpdate", function (self, elapsed)
 			end
 		end
 
+		if iCanSendToGuild and MsgToGuild then
+			cAnz = table.maxn(GuildNotifies)
+			if (cAnz > 0) then
+				currTime = time()
+				if (lastGTime == 0) then
+					lastGTime = currTime + 180
+					for i = 1, cAnz do
+						if (currTime >= GuildNotifies[i].CalendarAlarm) and
+							(currTime <= GuildNotifies[i].CalendarTime) then
+							leftTime = math.modf((GuildNotifies[i].CalendarTime - currTime) / 60)
+							if (leftTime >= 0) and (lastNotifyTimer ~= GuildNotifies[i].CalendarTime) then
+								AddonMessage(string.format("NOTIFYTIME-%d", GuildNotifies[i].CalendarTime))
+								GuildMessage(string.format(L["DlgEventStartsSoon"], GuildNotifies[i].EventTitle, leftTime))
+								iamSenderNow = true
+							end
+						end
+					end
+				elseif (currTime >= lastGTime) then
+					lastGTime = 0
+				end
+			end
+		end
+
 		checkTimers = false
 	end
 end)
 
+local function HelpCmds()
+	print(" ")
+	CPrint(L["HelpHeader"])
+	CPrint(L["HelpSendMsg"])
+	CPrint(L["HelpReset"])
+	print(" ")
+end
+
+local function SlashHandler(arg)
+	if ((arg ~= "") and (arg ~= nil)) then
+		if (arg:lower() == "sendmsg") then
+			toggleMsgToGuild()
+		elseif (arg:lower() == "reset") then
+			if checkTimers then
+				AddCommand("RESET", CommandQueue)
+				CPrint(L["Queued"])
+			else
+				iTimerCanStart = false
+				ReInitNotifies(EventNotifies)
+			end
+		else
+			HelpCmds()
+		end
+	else
+		HelpCmds()
+	end
+end
+
+SlashCmdList["CalEventNotifier"] = SlashHandler
+SLASH_CalEventNotifier1 = "/cen"
